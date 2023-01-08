c5783d3622e0a7e7aa55ac080cd96da0998a3b60
ShiYu <left.hand.lapis@gmail.com>
2023-01-08 13:19:08 +0900

    update below ・Implemented a count button for throwing things away ・Sorting css properties ・ Changed the entire wording

---
+++ src/src/components/countButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class CountButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {output: "捨て活スタート：手放した数は0個です！", count:0};
+    }
+  
+    buttonClick() {
+      const trashCount = this.state.count + 1
+      this.setState({output:"現在「" + trashCount + "」個の物を手放しています！", count:trashCount});
+    }
+  
+    render() {
+      return (
+        <div className="count-button-field">
+          <div>{this.state.output}</div>
+          <br/> 
+          <div>
+            <button className="trash-button" onClick={()=>{this.buttonClick()}}> 物を1つ捨てたら押してみよう！ </button>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default CountButton;
